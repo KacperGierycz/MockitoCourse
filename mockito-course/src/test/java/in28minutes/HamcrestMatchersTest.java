69bad32307e495c45b82c282a8359cc9c904e9f3
KacperGierycz <kacpergierycz@gmail.com>
2021-11-02 14:56:32 +0100

    HamCrastMatchers lot of matchers

--- mockito-course/src/test/java/in28minutes/HamcrestMatchersTest.java
+++ mockito-course/src/test/java/in28minutes/HamcrestMatchersTest.java
@@ -1,12 +1,20 @@
 package in28minutes;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayContainingInAnyOrder;
+
+import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
-
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.Test;
 
@@ -17,7 +25,23 @@ public class HamcrestMatchersTest {
 		List<Integer> scores = Arrays.asList(99,100,101,105);
 		//scores has 4 items
 		assertThat(scores, hasSize(4));
-		assertThat(scores, hasItems(99,100,106));
+		assertThat(scores, hasItems(99,100,105));
+		
+		//every item :>90
+		assertThat(scores, everyItem(greaterThan(90)));
+		assertThat(scores, everyItem(lessThan(190)));
+		
+		//String
+		assertThat("",isEmptyString());
+		assertThat(null, isEmptyOrNullString());
+		
+		// Arrays
+		Integer[] marks = {1,2,3};
+		
+		assertThat(marks,arrayWithSize(3));
+		assertThat(marks, arrayContaining(1,2,3));
+		assertThat(marks, arrayContainingInAnyOrder(2,1,3));
+		
 	}
 
 }
